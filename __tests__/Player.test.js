bcd4802dced25738cca2952fe345eef7f8c025d5
Ronan <galv.ronan@gmail.com>
2021-04-27 16:22:02 -0400

    Added Prototype Methods for Player.js.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion.js');
@@ -17,3 +17,25 @@ test('creates a player object', () => {
     );
     // console.log(player.inventory)
 })
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+// On player creation, the inventory should already have something in it, so a call to player.getInventory() should return an array.
+// There's also the case of an empty inventory needing to return false.
+// You can simulate an empty array yourself by setting player.inventory = [] before the next expect() runs.
