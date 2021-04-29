e9efda68818539b7022b244cbfacfac91dda5f5c
Ronan <galv.ronan@gmail.com>
2021-04-28 21:28:39 -0400

    player-object tests completed

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,30 @@ test("subtracts from player's health", () => {
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+// As mentioned previously, it's hard to test for randomness within a range.
+// That's why player.strength is set to 10
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThanOrEqual(oldCount);
+})
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
